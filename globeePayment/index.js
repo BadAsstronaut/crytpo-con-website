b03c0fe0fb8c6670523f7fa75dc9fe872cc971c6
Mickey <kyle.mickey@gmail.com>
2019-03-12 10:29:18 -0600

    cloudformation template is at a pseudo-stable state; starting to implement lambda functionality

--- globeePayment/index.js
+++ globeePayment/index.js
@@ -1,9 +1,14 @@
 'use strict';
 
-module.exports.globeePayment = (e, ctx, cb) => {
-    console.log(JSON.stringify(e), JSON.stringify(ctx));
+const aws = require('aws-sdk');
+const requester = require('request-promise-native');
 
-    cb({
+
+module.exports.globeePayment = (e, ctx, cb) => {
+    const dynamoClient = new aws.DynamoDB.DocumentClient();
+    const reqData = JSON.parse(e.body);
+
+    cb(null, {
         statusCode: 201,
         headers: {
             'x-custom-test': 'Test header'

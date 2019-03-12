265db19be098a67cc6df8a57c46f687172beaec3
Mickey <kyle.mickey@gmail.com>
2019-03-11 22:05:10 -0600

    Adding to lambda handler

--- globeePayment/globeePayment.js
+++ globeePayment/globeePayment.js
@@ -1,5 +1,17 @@
 'use strict';
 
-module.exports.globeePayment = event => {
-    return JSON.stringify(event);
+module.exports.globeePayment = (e, ctx, cb) => {
+    console.log(JSON.stringify(e), JSON.stringify(ctx));
+
+    cb({
+        statusCode: 201,
+        headers: {
+            'x-custom-test': 'Test header'
+        },
+        body: JSON.stringify({
+            status: 'good',
+            redirect_url: 'http://redirect.to.here',
+            success_url: 'http://success.to.here',
+        }),
+    });
 };
